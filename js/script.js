f1c032c1c19880b107ec9d05660ed041ee580709
Diogo Santos <diogo_f_s_@hotmail.com>
2023-10-20 18:11:11 -0300

    Dark Mode

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-let darkModeState = false
+function changeMode() {
+    var element = document.getElementById("mainBody")
+    if (element.classList.contains("bodyLight")){
+        element.classList.remove("bodyLight")
+        element.classList.add("bodyDark")
+        this.changeColorLetters(true)
+        this.changeClassButtons(true)
+        return;
+    }
+    element.classList.remove("bodyDark")
+    element.classList.add("bodyLight")
+    this.changeColorLetters(false)
+    this.changeClassButtons(false)
+}
+
+function changeColorLetters(darkMode){
+    var elements = document.getElementsByClassName("divText")
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].classList.add(darkMode == true ? "letterLight" : "letterBlack")
+        elements[i].classList.remove(darkMode == false ? "letterLight" : "letterBlack")
+    }
+}
+
+function changeClassButtons(darkMode){
+    var elements = document.getElementsByClassName("removeBorderAndBackgroundColor")
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].classList.add(darkMode == true ? "headerButtonDark" : "headerButtonLight")
+        elements[i].classList.remove(darkMode == false ? "headerButtonDark" : "headerButtonLight")
+    }
+}
