89186eb2a5590a03938edc1caeebbcf9f7287c7c
Diogo Santos <diogo_f_s_@hotmail.com>
2023-10-27 20:47:02 -0300

    Close menu button

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function openMenu() {
 
 function closeMenu () {
     var element = document.getElementById("menuMobile")
+    element.classList.remove("visible")
     element.classList.add("invisible")
 }
 
